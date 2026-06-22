853f77edaac2213d1e14b0e2b4b98d63a17ba413
Samuel Le <robbo_rules1@live.com.au>
2026-06-22 19:15:54 +1000

    Camera controller for map

---
+++ Nataterra/Assets/_Project/Features/CameraSystem/Scripts/CameraManager.cs
@@ -0,0 +1,21 @@
+using System.Drawing;
+using TGS;
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraManager : MonoBehaviour
+{
+
+
+
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
