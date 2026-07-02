8a29056e624f54ce7251bfaff247a08e9e34641c
Samuel Le <robbo_rules1@live.com.au>
2026-07-02 18:29:43 +1000

    Added unit stats

--- Nataterra/Assets/_Project/Features/Faction/Scripts/UnitData.cs
+++ Nataterra/Assets/_Project/Features/Faction/Scripts/UnitData.cs
@@ -7,4 +7,11 @@ public class UnitData : ScriptableObject
 {
     public UnitType Unit;
     public GameObject Prefab;
+
+    public int Attack;
+    public int Armour;
+    public int Health;
+    public int Initiative;
+    public int Group;
+    public int Movement;
 }
--- Nataterra/Assets/_Project/Features/Gameplay/Unit.cs
+++ Nataterra/Assets/_Project/Features/Gameplay/Unit.cs
@@ -5,9 +5,22 @@ public class Unit
 {
     public string GUID;
 
+    public int Attack;
+    public int Armour;
+    public int Health;
+    public int Initiative;
+    public int Group;
+    public int Movement;
 
     public Unit(UnitData data)
     {
         GUID = Guid.NewGuid().ToString();
+
+        Attack = data.Attack;
+        Armour = data.Armour;
+        Health = data.Health;
+        Initiative = data.Initiative;
+        Group = data.Group;
+        Movement = data.Movement;
     }
 }
